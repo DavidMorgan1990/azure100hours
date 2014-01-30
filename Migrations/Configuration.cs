0ed675ff579434c3b9c7c11309d04ca8e7ef77af
David Morgan <nagrom.1990@gmail.com>
2014-01-30 13:59:38 +0000

    first commit

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,55 @@
+namespace Test100Hours3.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class MUConfiguration : DbMigrationsConfiguration<Test100Hours3.MuDatabaseEntities>
+    {
+        public MUConfiguration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Test100Hours3.MuDatabaseEntities context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Test100Hours3.aspnetEntities>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Test100Hours3.aspnetEntities context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
